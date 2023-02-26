7d209bb02d28ca879bb9600d909c0751d12fd094
Luis Camargo <luismiguelcamar@gmail.com>
2023-02-26 01:58:57 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const suits = ["♦", "♥", "♠", "♣"];
+const numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomSuit = Math.floor(Math.random() * suits.length);
+
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+
+  let suitTop = document.querySelector(".top-suit");
+  suitTop.innerHTML = suits[randomSuit];
+
+  let suitBottom = document.querySelector(".bottom-suit");
+  suitBottom.innerHTML = suits[randomSuit];
+
+  let cardValue = document.querySelector(".number");
+  cardValue.innerHTML = numbers[randomNumber];
+
+  if (randomSuit <= 1) {
+    suitTop.style.color = "red";
+    suitBottom.style.color = "red";
+  }
 };
